ca4b0c893959198706b0ca301d3e87a87dbc7081
grit1 <grit1life@gmail.com>
2020-09-14 15:31:35 +0900

    spring boot first configuration

---
+++ src/main/java/tk/spring/doseo/DoseoApplication.java
@@ -0,0 +1,13 @@
+package tk.spring.doseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/spring/doseo/DoseoApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.spring.doseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
